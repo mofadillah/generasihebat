820f95aacaa950846e89420368a0b9a6410e3f97
Fadillah Hadiyanto <mochamad.fadillah@btpn.com>
2020-01-21 22:20:42 +0700

    Devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: :testing
+
   def index
   end
 
   def about
+  end
+
+  def testing
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#index'
   get 'about', to: 'pages#about'
+  get 'testing', to: 'pages#testing'
   get 'article', to: 'articles#index'
   get 'article/:id', to: 'articles#detail'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
