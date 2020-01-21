c7763f8dd5d787de23ff1b09ea3ea8af7260a623
Fadillah Hadiyanto <mochamad.fadillah@btpn.com>
2020-01-21 18:04:27 +0700

    Article Prepare

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,7 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+
+  def detail
+  end
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: {minimum: 3}
   validates :slug, presence: true
-  validates :content, presence: true
+  validates :content, presence: true, length: {minimum: 10}
+  validates :thumbnail, presence: true
 end
